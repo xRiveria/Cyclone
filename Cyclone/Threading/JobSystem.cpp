0741faeadd616551950d49064f741295c487f3bc
Ryan Tan <ryan-wende@outlook.com>
2024-08-02 15:03:39 +0800

    [Windows] Setting Up of Affinity Masks with Prioritization, [Build] Removal of Legacy Code

--- Cyclone/Threading/JobSystem.cpp
+++ Cyclone/Threading/JobSystem.cpp
@@ -6,165 +6,11 @@
 #include <assert.h>
 #include <winerror.h>
 #include <deque>
-
-namespace JobSystem
-{
-    bool JobSystem::Initialize()
-    {
-        // Retrieve the total number of hardware threads in the system.
-        m_ThreadCountTotal = std::thread::hardware_concurrency();
-
-        // Calculate the actual number of worker threads we want (-1 for the main thread, or any subsystems we wish to reserve a thread for).
-        m_ThreadCountSupported = m_ThreadCountTotal - 1;
-        m_ThreadNames[std::this_thread::get_id()] = "Main";
-
-        // Create all our worker threads and put them to work.
-        for (uint32_t threadID = 0; threadID < m_ThreadCountSupported; threadID++)
-        {
-            std::thread worker([this, threadID]
-            {
-                while (true)
-                {
-                    if (!TaskLoop())
-                    {
-                        // No tasks avaliable.
-                        std::unique_lock<std::mutex> lock(m_WakeMutex);
-                        m_WakeCondition.wait(lock); // Unlock and allow execution to proceed once condition is met.
-                    }
-                }
-            });
-
-            m_ThreadNames[worker.get_id()] = "Worker_" + std::to_string(threadID);
-
-            // Platform specific setup. We will use the following to name our threads officially, allowing them to show up in Visual Studio Debugger.
-            HANDLE hHandle = (HANDLE)worker.native_handle();
-            std::wstringstream wideStringName;
-            wideStringName << m_ThreadNames[worker.get_id()].c_str();
-            SetThreadDescription(hHandle, wideStringName.str().c_str());
-
-            worker.detach(); // Seperates the thread of execution, allowing execution to continue independantly. https://stackoverflow.com/questions/22803600/when-should-i-use-stdthreaddetache5
-        }
-
-        return true;
-    }
-
-    // The meat of our library. This function runs across all threads and is used to execute our jobs.
-    bool JobSystem::TaskLoop()
-    {
-        Job job;
-        if (m_JobQueue.pop_front(job))
-        {
-            JobInformation jobInformation = {};
-            jobInformation.m_GroupID = job.m_GroupID;
-
-            for (uint32_t i = job.m_GroupJobOffset; i < job.m_GroupJobEnd; ++i)
-            {
-                jobInformation.m_JobIndex = i;
-                jobInformation.m_GroupIndex = i - job.m_GroupJobOffset;
-                jobInformation.m_IsFirstJobInGroup = (i == job.m_GroupJobOffset);
-                jobInformation.m_IsLastJobInGroup = (i == job.m_GroupJobEnd - 1);
-                job.m_Job(jobInformation);
-            }
-
-            m_Counter.fetch_sub(1);
-            return true;
-        }
-
-        return false;
-    }
-
-    uint32_t JobSystem::CalculateDispatchJobCount(uint32_t jobCount, uint32_t groupSize)
-    {
-        // Calculates the amount of jobs to dispatch for this specific task.
-        return (jobCount + groupSize - 1) / groupSize;
-    }
-
-    void JobSystem::Dispatch(uint32_t jobCount, uint32_t groupSize, const std::function<void(JobInformation)>& jobInformation)
-    {
-        if (jobCount == 0 || groupSize == 0)
-        {
-            return;
-        }
-
-        const uint32_t groupCount = CalculateDispatchJobCount(jobCount, groupSize); // Tells us how many worker threads (or groups) will be activated to handle the queue.
-
-        // Update context state.
-        m_Counter.fetch_add(groupCount);
-
-        Job job;
-        job.m_Job = jobInformation;
-
-        for (uint32_t groupID = 0; groupID < groupCount; ++groupID)
-        {
-            // For each group, generate one job to handle it.
-            job.m_GroupID = groupID;
-            job.m_GroupJobOffset = groupID * groupSize;
-            job.m_GroupJobEnd = std::min(job.m_GroupJobOffset + groupSize, jobCount); // The last task belonging to this job.
-
-            // Try to push this job until it is pushed successfully.
-            while (!m_JobQueue.push_back(job))
-            {
-                m_WakeCondition.notify_all();
-                TaskLoop();
-            }
-        }
-
-        // Wake up any threads that might be sleeping.
-        m_WakeCondition.notify_all();
-    }
-
-    void JobSystem::Execute(const std::function<void(JobInformation)>& jobInformation)
-    {
-        m_Counter.fetch_add(1);
-
-        Job job;
-        job.m_Job = jobInformation;
-        job.m_GroupID = 0;
-        job.m_GroupJobOffset = 0;
-        job.m_GroupJobEnd = 1;
-
-        // Try to push a new job. If we're unable to do so, we continue to wake threads up to execute existing tasks to make space for the new job.
-        while (!m_JobQueue.push_back(job))
-        {
-            m_WakeCondition.notify_all();
-            TaskLoop();
-        }
-
-        // Once pushed, we wake up a thread to execute the pushed thread.
-        m_WakeCondition.notify_one();
-    }
-
-    bool JobSystem::IsBusy()
-    {
-        // Whenever the context label is greater than 0, it means there are tasks to be completed.
-        return m_Counter.load() > 0;
-    }
-
-    void JobSystem::Wait()
-    {
-        // Wake any threads that might be sleeping to execute all tasks.
-        m_WakeCondition.notify_all();
-
-        // Waiting will also put the current thread to good use by working on a job if it can. Disable this to not make our main thread work.
-        if (m_UseMainThreadForTasks)
-        {
-            TaskLoop();
-        }
-    }
-
-    uint32_t JobSystem::GetThreadCountAvaliable()
-    {
-        if (IsBusy())
-        {
-            return m_ThreadCountSupported - m_Counter.load();
-        }
-
-        return m_ThreadCountSupported;
-    }
-}
+#include <iostream>
 
 namespace Cyclone
 {
+    // Denotes a single task (or function call) submitted by the user either with Execution() or Dispatch() as part of a larger work group.
     struct Job
     {
 
@@ -320,8 +166,97 @@ namespace Cyclone
                 std::thread::native_handle_type threadHandle = workerThread.native_handle();
                 int coreID = threadID + 1;
 
-                /// Naming
+                if (priorityType == Priority::Streaming)
+                {
+                    // Put streaming on the last core.
+                    // The second thread with ID 1 with streaming prioprity is assigned to the second last core at N - 1 - 1.
+                    coreID = g_InternalState->m_CoreCount - 1 - threadID; 
+                }
+
+#ifdef _WIN32
+                // Put each thread on a dedicated core.
+                DWORD_PTR affinityMask = 1ull << coreID;
+                DWORD_PTR affinityResult = SetThreadAffinityMask(threadHandle, affinityMask);
+                assert(affinityResult > 0);
+
+                if (priorityType == Priority::High)
+                {
+                    BOOL priorityResult = SetThreadPriority(threadHandle, THREAD_PRIORITY_NORMAL);
+                    assert(priorityResult != 0);
+
+                    std::wstring threadName = L"Cyclone::HighPriorityJobThread_" + std::to_wstring(threadID);
+                    HRESULT namingResult = SetThreadDescription(threadHandle, threadName.c_str());
+                    assert(SUCCEEDED(namingResult));
+                }
+                else if (priorityType == Priority::Low)
+                {
+                    BOOL priorityResult = SetThreadPriority(threadHandle, THREAD_PRIORITY_LOWEST);
+                    assert(priorityResult != 0);
+
+                    std::wstring threadName = L"Cyclone::LowPriorityJobThread_" + std::to_wstring(threadID);
+                    HRESULT namingResult = SetThreadDescription(threadHandle, threadName.c_str());
+                    assert(SUCCEEDED(namingResult));
+                }
+                else if (priorityType == Priority::Streaming)
+                {
+                    BOOL priorityResult = SetThreadPriority(threadHandle, THREAD_PRIORITY_LOWEST);
+                    assert(priorityResult != 0);
+
+                    std::wstring threadName = L"Cyclone::StreamingLowPriorityJobThread_" + std::to_wstring(threadID);
+                    HRESULT namingResult = SetThreadDescription(threadHandle, threadName.c_str());
+                    assert(SUCCEEDED(namingResult));
+                }
+#endif               
             }
         }
+
+        // Post message to logging system.
+        char logMessage[256] = {};
+        snprintf(logMessage, sizeof(logMessage), "Cyclone initialized with %d cores\n\tHigh priority threads: %d\n\tLow priority threads: %d\n\tStreaming threads: %d", g_InternalState->m_CoreCount, GetThreadCount(Priority::High), GetThreadCount(Priority::Low), GetThreadCount(Priority::Streaming));
+        std::cout << logMessage << "\n";
+    }
+
+    void Shutdown()
+    {
+        g_InternalState->Shutdown();
+        delete g_InternalState;
+    }
+
+    uint32_t GetThreadCount(Priority priorityType)
+    {
+        return g_InternalState->m_Resources[int(priorityType)].m_ThreadCount;
+    }
+
+    bool IsBusy(const Context& executionContext)
+    {
+        return executionContext.m_JobCounter.load() > 0; // m_JobCounter denotes the number of jobs that still needs to be executed.
+    }
+
+    void Wait(const Context& executionContext)
+    {
+        if (IsBusy(executionContext))
+        {
+            PriorityResources& resource = g_InternalState->m_Resources[int(executionContext.m_Priority)];
+
+            // Wake any threads that might be sleeping.
+            resource.m_WakeCondition.notify_all();
+
+            // Work() will pick up any jobs that are still waiting and execute them on this thread.
+            resource.Work(resource.m_NextQueueIndex.fetch_add(1) % resource.m_ThreadCount);
+
+            while (IsBusy(executionContext))
+            {
+                // If we're here, it means there are jobs that couldn't be picked up by this thread.
+                // In this case, it means there are jobs that are in the process of executing.
+                // We will allow the thread to be swapped out by the OS to avoid spinning endlessly.
+                std::this_thread::yield();
+            }
+        }
+    }
+
+    uint32_t GetDispatchGroupCount(uint32_t jobCount, uint32_t groupSize)
+    {
+        // Calculates the amount of job groups to dispatch. We will overestimate here.
+        return (jobCount + groupSize - 1) / groupSize;
     }
 }
--- Cyclone/Threading/JobSystem.h
+++ Cyclone/Threading/JobSystem.h
@@ -4,86 +4,8 @@
 #include <atomic>
 #include <condition_variable>
 #include <unordered_map>
+// The RingBuffer and SpinLock classes have been phased out to allow for a more streamlined implementation.
 #include "RingBuffer.h"
-
-/* == Threading ==
-
-    A task is defined as any function or workload the engine/user wishes to execute asychronously. This could be input polling, UI updates, system initialization or even rendering. Jobs
-    are generated to fulfill a task, which are then grouped up and executed in parallel in our threads.
-
-    We currently supported singular (function) and loop based parallization.
-
-    When notify_all() is called, our threads are woken up and will proceed to look for tasks serially, picking up and executing jobs found. If no jobs are found, they return to a perpertual waiting state.
-*/
-
-namespace JobSystem
-{
-    struct JobInformation
-    {
-        uint32_t m_JobIndex;      // Job index relative to dispatch.
-        uint32_t m_GroupID;       // Group index relative to dispatch.
-        uint32_t m_GroupIndex;    // Job index relative to group.
-        bool m_IsFirstJobInGroup; // Is the current job the first one in the group?
-        bool m_IsLastJobInGroup;  // Is the current job the last one in the group?
-    };
-
-    struct Job
-    {
-        std::function<void(JobInformation)> m_Job;
-        uint32_t m_GroupID = 0;
-        uint32_t m_GroupJobOffset = 0;
-        uint32_t m_GroupJobEnd = 0;
-    };
-
-    class JobSystem
-    {
-    public:
-        bool Initialize(); // Creates our internal resources such as worker threads.
-
-        /* == Dispatch ==
-        *
-            Divides a single task into multiple jobs and execute them in parallel.
-
-            - Job Count: The amount of jobs to generate for this thread.
-            - Group Size: How many jobs to execute per thread. Jobs inside a group execute serially. Any idle thread will execute this job.
-            - Job Information: Receives a JobInformation struct as a parameter.
-        */
-
-        void Dispatch(uint32_t jobCount, uint32_t groupSize, const std::function<void(JobInformation)>& jobInformation);
-        void Execute(const std::function<void(JobInformation)>& jobInformation); // Adds a task to execute asynchronously. Any idle thread will execute this job.
-
-        bool IsBusy(); // Allows the main thread to check if any worker threads are busy executing jobs.
-        void Wait();   // Wait until all threads become idle.
-
-        uint32_t GetThreadCount() const { return m_ThreadCountTotal; }
-        uint32_t GetThreadCountSupported() const { return m_ThreadCountSupported; }
-        uint32_t GetThreadCountAvaliable();
-
-        uint32_t GetQueuedTasksCount() const { return m_Counter.load(); }
-
-        bool IsMainThreadUtilitizedForTasks() const { return m_UseMainThreadForTasks; }
-        void UseMainThreadForTasks(bool value) { m_UseMainThreadForTasks = value; } // Not recommended as it can affect your current program.
-
-    private:
-        bool TaskLoop();
-        uint32_t CalculateDispatchJobCount(uint32_t jobCount, uint32_t groupSize);
-
-    public:
-        std::unordered_map<std::thread::id, std::string> m_ThreadNames;
-
-    private:
-        bool m_UseMainThreadForTasks = false;
-
-        uint32_t m_ThreadCountTotal = 0;         // Total thread count for our system.
-        uint32_t m_ThreadCountSupported = 0;     // -1 for the main thread. We can also reserve certain threads for certain systems.
-
-        std::condition_variable m_WakeCondition; // Used with g_WakeMutex. Worker threads simply sleep when there are no jobs and can be waken up with this condition.
-        std::mutex m_WakeMutex;                  // As above.
-
-        std::atomic<uint32_t> m_Counter{ 0 };    // Defines a state of execution. Can be waited on. This tells us how many threads must finish their tasks for the threading library to become idle.
-        RingBuffer<Job, 256> m_JobQueue;         // A thread safe queue to put pending jobs onto. This has a capacity of 256 jobs. A worker thread can grab a job from the end of the queue, and new tasks are appended to the start.
-    };
-}
 
 // The engine does not know about the concept of Jobs. It simply is concerned with adding tasks that need to be executed in parallel.
 namespace Cyclone
